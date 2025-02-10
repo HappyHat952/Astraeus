b039f8ebec0f98f6c481de210b22507783265e36
elamousin <e.lamousin@gmail.com>
2025-02-10 09:06:00 -0500

    hello

--- src/teams/student/testPlotz/analysis/ResourceManager.java
+++ src/teams/student/testPlotz/analysis/ResourceManager.java
@@ -3,9 +3,12 @@ package teams.student.testPlotz.analysis;
 import objects.entity.node.Node;
 import objects.entity.unit.Unit;
 import objects.resource.Resource;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ResourceManager {
 
@@ -50,7 +53,7 @@ public class ResourceManager {
             return null;
         }
         for(Node n: nodes) {
-            if (teams.student.plotz.ResourceManager.isSafe(n)) {
+            if (isSafe(n)) {
                 safeNodes.add(n);
             }
         }
@@ -61,7 +64,7 @@ public class ResourceManager {
     public static boolean isSafe(Resource r) {
         ArrayList<Unit> enemies = OverallAnalysis.getEnemy().getFighters();
         for(Unit u: enemies) {
-            if (u.getDistance(r) < 3000) {
+            if (u.getDistance(r) < 2500) {
                 return false;
             }
         }
@@ -72,11 +75,34 @@ public class ResourceManager {
         ArrayList<Unit> enemies = OverallAnalysis.getEnemy().getFighters();
 
         for(Unit u: enemies) {
-            if (u.getDistance(n) < 3000) {
+            if (u.getDistance(n) < 2500) {
                 return false;
             }
         }
         return true;
+    }
+
+    public static void draw(Graphics g) {
+        if (getSafeResources() != null) {
+
+            for (Resource r : getSafeResources()) {
+                if (!r.isPickedUp()) {
+                    g.setColor(Color.green);
+                    g.fillOval(r.getX(), r.getY(), 20, 20);
+                }
+            }
+        }
+        for (Resource r: Objects.requireNonNull(takenResources)) {
+            if (r != null && !r.isPickedUp()) {
+                g.setColor(Color.yellow);
+                g.fillOval(r.getX(), r.getY(), 20, 20);
+            }
+        }
+        for (Node n: Objects.requireNonNull(getSafeNodes())) {
+            g.setColor(Color.green);
+            g.drawOval(n.getX(), n.getY(), 20, 20);
+        }
+
     }
 
 }
